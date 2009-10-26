ea91f59fc8e7d312d458009c868e8b546af49bf0
Jose Pedro Magalhaes <jpm@cs.ox.ac.uk>
2009-10-26 10:18:23 +0000

    Update the author (CREDITS lists all the contributions), more documentation.

--- src/Generics/Regular.hs
+++ src/Generics/Regular.hs
@@ -33,75 +33,100 @@
 --
 -- An instance of @Regular@ can be derived automatically with TH by invoking:
 --
--- >  $(deriveAll ''Logic "PFLogic")
--- >  type instance PF Logic = PFLogic
+-- > $(deriveAll ''Logic "PFLogic")
+-- > type instance PF Logic = PFLogic
 --
 -- We define some logic expressions:
 --
--- >  l1, l2, l3 :: Logic
--- >  l1 = Var "p"
--- >  l2 = Not l1
--- >  l3 = l1 :->: l2
+-- > l1, l2, l3 :: Logic
+-- > l1 = Var "p"
+-- > l2 = Not l1
+-- > l3 = l1 :->: l2
 --
 -- And now we can use all of the generic functions. Flattening:
 --
--- >  ex0 :: [Logic]
--- >  ex0 = flattenr (from l3)
+-- > ex0 :: [Logic]
+-- > ex0 = flattenr (from l3)
+-- >
 -- > > [Var "p",Not (Var "p")]
 --
 -- Generic equality:
 --
--- >  ex1, ex2 :: Bool
--- >  ex1 = eq l3 l3
+-- > ex1, ex2 :: Bool
+-- > ex1 = eq l3 l3
+-- >
 -- > > True
 -- >
--- >  ex2 = eq l3 l2
+-- >
+-- > ex2 = eq l3 l2
+-- >
 -- > > False
 --
 -- Generic show:
 --
--- >  ex3 :: String
--- >  ex3 = G.show l3
+-- > ex3 :: String
+-- > ex3 = G.show l3
+-- >
 -- > > "((:->:) (Var \"p\") (Not (Var \"p\")))"
 --
 -- Generic read:
 --
--- >  ex4 :: Logic
--- >  ex4 = G.read ex3
+-- > ex4 :: Logic
+-- > ex4 = G.read ex3
+-- >
 -- > > Var "p" :->: Not (Var "p")
 --
 -- Value generation:
 --
--- >  ex5, ex6 :: Logic
--- >  ex5 = left
+-- > ex5, ex6 :: Logic
+-- > ex5 = left
+-- >
 -- > > Var ""
 -- >
--- >  ex6 = right
+-- >
+-- > ex6 = right
+-- >
 -- > > F
 --
 -- Folding:
 --
--- >  ex7 :: Bool
--- >  ex7 = fold (alg (\_ -> False)) l3 where
--- >  alg env = (env & impl & (==) & (&&) & (||) & not & True & False)
--- >  impl p q = not p || q
+-- > ex7 :: Bool
+-- > ex7 = fold (alg (\_ -> False)) l3 where
+-- >   alg env = (env & impl & (==) & (&&) & (||) & not & True & False)
+-- >   impl p q = not p || q
+-- >
 -- > > True
 --
 -- Unfolding:
 --
--- >  ex8 :: Logic
--- >  ex8 = unfold alg 8 where
--- >    alg :: CoAlgebra Logic Int
--- >    alg n | odd n || n <= 0 = Left ""
--- >          | even n          = Right (Left (n-1,n-2))
+-- > ex8 :: Logic
+-- > ex8 = unfold alg 8 where
+-- >   alg :: CoAlgebra Logic Int
+-- >   alg n | odd n || n <= 0 = Left ""
+-- >         | even n          = Right (Left (n-1,n-2))
+-- >
 -- > > Var "" :->: (Var "" :->: (Var "" :->: (Var "" :->: Var "")))
+--
+-- Constructor names:
+--
+-- > ex9 = conNames (undefined :: Logic)
+-- >
+-- > > ["Var",":->:",":<->:",":&&:",":||:","Not","T","F"]
+--
+-- Deep seq:
+--
+-- > ex10 = gdseq (Not (T :->: (error "deep seq works"))) ()
+-- >
+-- > > *** Exception: deep seq works
 -- 
 -----------------------------------------------------------------------------
 
 module Generics.Regular (
     module Generics.Regular.Base,
-    module Generics.Regular.TH
+    module Generics.Regular.TH,
+    module Generics.Regular.Functions
   ) where
 
 import Generics.Regular.Base
 import Generics.Regular.TH
+import Generics.Regular.Functions
